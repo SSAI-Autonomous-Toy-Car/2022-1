1bf4a94677eed134b6fd91d969d7edd218659273
Deepracer <hong99z@snu.ac.kr>
2022-07-02 03:42:40 -0400

    add deepracer_ws

---
+++ deepracer_ws/aws-deepracer-launcher/build/inference_pkg/CMakeFiles/inference_node.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/deepracer_ws/aws-deepracer-launcher/aws-deepracer-inference-pkg/inference_pkg/src/image_process.cpp" "/root/deepracer_ws/aws-deepracer-launcher/build/inference_pkg/CMakeFiles/inference_node.dir/src/image_process.cpp.o"
+  "/root/deepracer_ws/aws-deepracer-launcher/aws-deepracer-inference-pkg/inference_pkg/src/inference_node.cpp" "/root/deepracer_ws/aws-deepracer-launcher/build/inference_pkg/CMakeFiles/inference_node.dir/src/inference_node.cpp.o"
+  "/root/deepracer_ws/aws-deepracer-launcher/aws-deepracer-inference-pkg/inference_pkg/src/intel_inference_eng.cpp" "/root/deepracer_ws/aws-deepracer-launcher/build/inference_pkg/CMakeFiles/inference_node.dir/src/intel_inference_eng.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEFAULT_RMW_IMPLEMENTATION=rmw_fastrtps_cpp"
+  "RCUTILS_ENABLE_FAULT_INJECTION"
+  "SPDLOG_COMPILED_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/root/deepracer_ws/aws-deepracer-launcher/aws-deepracer-inference-pkg/inference_pkg/include"
+  "/usr/include/opencv4"
+  "/home/deepracer/intel/openvino_2022/runtime/include/ie"
+  "/root/deepracer_ws/aws-deepracer-launcher/install/deepracer_interfaces_pkg/include"
+  "/opt/ros/foxy/include"
+  "/home/deepracer/intel/openvino_2022/runtime/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
